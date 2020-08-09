72de62ee757e630684eb516e5a8b4d0f4bd3189d
abuu <abuu@DESKTOP-HK9E16D.localdomain>
2020-08-09 22:50:00 +0500

    Подключит декомпозированные редьюсеры и Thunk в приложение

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
-import {reducer} from "./reducer.js";
-import {ActionCreator} from "./reducer.js";
+import thunk from "redux-thunk";
+import reducer from "./reducer/reducer.js";
+import {Operation as DataOperation} from "./reducer/data/data.js";
+import {Operation as UserOperation, ActionCreator, AuthorizationStatus} from "./reducer/user/user.js";
+import {createAPI} from "./api.js";
 import {generateFilms} from './mock/film';
 
-const promoFilm = {
-  name: `The Grand Budapest Hotel`,
-  genre: `Drama`,
-  released: 2014,
-  previewImage: `img/bohemian-rhapsody.jpg`,
-  videoLink: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
-};
+const api = createAPI(() => {});
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    applyMiddleware(thunk.withExtraArgument(api))
 );
-
-store.dispatch(ActionCreator.setFilms(generateFilms(10)));
-store.dispatch(ActionCreator.setPromoFilm(promoFilm));
 
 ReactDOM.render(
     <Provider store={store}>
